ad435b08382dbcf02fc9d92be40acba6487a1168
sAhA123riAr <95167395+sijad1@users.noreply.github.com>
2022-02-05 16:59:15 +0600

    Add files via upload

---
+++ SS-vau.py
@@ -0,0 +1,243 @@
+import sys
+import time
+
+time.sleep(1)
+
+import os
+end="\033[0m"
+blue="\33[94m"
+red="\033[91m"
+lightblue="\033[94m"
+white="\33[97m"
+yellow="\33[93m"
+green="\033[1;32m"
+cyan="\033[96m"
+end="\033[0m"
+os.system('clear')
+time.sleep(2)
+print(lightblue+""" 
+  _                 _       
+ | |               (_)      
+ | |     ___   __ _ _ _ __  
+ | |    / _ \ / _` | | '_ \ 
+ | |___| (_) | (_| | | | | |
+ |______\___/ \__, |_|_| |_|
+               __/ |        
+              |___/         
+
+          
+""")
+a="ls"
+b="ls"
+inpa=str(input(lightblue+"Enter Username[>]"))
+inpb=str(input(lightblue+"Enter Passwd[>]"))
+if a==inpa and b==inpb:
+  print(green+"successful login [√]")
+  time.sleep(1)
+  os.system('clear')
+  pass 
+else:
+  print(red+"[×] Wrong uernem or Passwd")
+  time.sleep(2)
+  sys.exit()
+ 
+
+print(yellow+"""
+ __    __   _______  __       __        ______   ____    __    ____ 
+|  |  |  | |   ____||  |     |  |      /  __  \  \   \  /  \  /   / 
+|  |__|  | |  |__   |  |     |  |     |  |  |  |  \   \/    \/   /  
+|   __   | |   __|  |  |     |  |     |  |  |  |   \            /   
+|  |  |  | |  |____ |  `----.|  `----.|  `--'  |    \    /\    /    
+|__|  |__| |_______||_______||_______| \______/      \__/  \__/     
+                                                                    
+""")
+time.sleep(2)
+
+
+print(cyan+          "[1)Install All pkg")
+print(cyan+          "[2]View SHS Logo")
+print(cyan+          "[3] Contact with SHS vau social ")
+print(cyan+          "[4]Contact Kaif vau FB")
+print(cyan+          "[5]Others tools")
+print(red+            "[6]Exit")
+
+a=input(green+"Enter your Choice:")
+if '5' in a:
+  
+  time.sleep(1)
+  os.system('clear')
+  
+  print(red+""""
+                                                
+I8,        8        ,8I            88           
+`8b       d8b       d8'            ""    ,d     
+ "8,     ,8"8,     ,8"                   88     
+  Y8     8P Y8     8P  ,adPPYYba,  88  MM88MMM  
+  `8b   d8' `8b   d8'  ""     `Y8  88    88     
+   `8a a8'   `8a a8'   ,adPPPPP88  88    88     
+    `8a8'     `8a8'    88,    ,88  88    88,    
+     `8'       `8'     `"8bbdP"Y8  88    "Y888  
+                                                
+                                                
+""")
+  time.sleep(1)
+  
+  print(green+"[+]Install all pkg....")
+  time.sleep(4)
+  print("                        ")
+  print(cyan+"[+]pkg installing...")
+  time.sleep(6)
+  
+  
+  
+  os.system('clear')
+  time.sleep(2)
+  
+  
+  
+  print(cyan+"""
+ (   (                            
+ )\ ))\ )            (            
+(()/(()/(    (   (   )\       (   
+ /(_))(_))__ )\  )((((_)(     )\  
+(_))(_))|___((_)((_)\ _ )\ _ ((_) 
+/ __/ __|   \ \ / /(_)_\(_) | | | 
+\__ \__ \    \ V /  / _ \ | |_| | 
+|___/___/     \_/  /_/ \_\ \___/  
+                                  
+""")
+  print(cyan+"====================================")
+  print(yellow+          "[1]Phising git clone link")
+  print(yellow+          "[2]Fb id clone git clone link")
+  print(yellow+          "[3]Free fire Phising link")
+  print(yellow+          "[4]All pkg install git clone ")
+  print(cyan+"Exit")
+  e=input(green+"Enter your Choice:")
+  if '1' in e:
+    
+    time.sleep(2)
+    print("                        ")
+    print(green+           "[git clone]>    https://github.com/htr-tech/zphisher      <")
+    print("                ")
+    
+    sys.exit()
+  elif '2' in e:
+    print("wait.....")
+    time.sleep(3)
+    print("hellow")
+    
+    os.system("xdg-open https://github.com/Azim-vau/crack-pro.git")
+    sys.exit()
+  
+  if '3' in e:
+    time.sleep(2)
+    os.system('clear')
+    time.sleep(2)
+    
+    print("wait.....")
+    
+    os.system("xdg-open https://github.com/OnlineHacKing/FreeFire-Phishing.git")
+    sys.exit()
+    
+    
+    
+    
+    
+    
+    
+if '1' in a:
+  pritn(red+          "This pkg not avilable now [×]")
+  time.sleep(2)
+  sys.exit()
+  
+
+if '2' in a:
+  os.system('clear')
+  time.sleep(2)
+  print(green+"""
+  ██████  ▄▄▄       ██░ ██  ▄▄▄       ██▀███   ██▓ ▄▄▄       ██▀███  
+▒██    ▒ ▒████▄    ▓██░ ██▒▒████▄    ▓██ ▒ ██▒▓██▒▒████▄    ▓██ ▒ ██▒
+░ ▓██▄   ▒██  ▀█▄  ▒██▀▀██░▒██  ▀█▄  ▓██ ░▄█ ▒▒██▒▒██  ▀█▄  ▓██ ░▄█ ▒
+  ▒   ██▒░██▄▄▄▄██ ░▓█ ░██ ░██▄▄▄▄██ ▒██▀▀█▄  ░██░░██▄▄▄▄██ ▒██▀▀█▄  
+▒██████▒▒ ▓█   ▓██▒░▓█▒░██▓ ▓█   ▓██▒░██▓ ▒██▒░██░ ▓█   ▓██▒░██▓ ▒██▒
+▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒▓ ░▒▓░░▓   ▒▒   ▓▒█░░ ▒▓ ░▒▓░
+░ ░▒  ░ ░  ▒   ▒▒ ░ ▒ ░▒░ ░  ▒   ▒▒ ░  ░▒ ░ ▒░ ▒ ░  ▒   ▒▒ ░  ░▒ ░ ▒░
+░  ░  ░    ░   ▒    ░  ░░ ░  ░   ▒     ░░   ░  ▒ ░  ░   ▒     ░░   ░ 
+      ░        ░  ░ ░  ░  ░      ░  ░   ░      ░        ░  ░   ░     
+                                                                     
+""")
+  print(".")
+  sys.exit()
+ 
+
+
+if '3' in a:
+  print(green+"wait.......")
+  
+  time.sleep(2)
+  os.system('clear')
+  print(green+"""
+███████╗ ██████╗  ██████╗██╗ █████╗ ██╗         
+██╔════╝██╔═══██╗██╔════╝██║██╔══██╗██║         
+███████╗██║   ██║██║     ██║███████║██║         
+╚════██║██║   ██║██║     ██║██╔══██║██║         
+███████║╚██████╔╝╚██████╗██║██║  ██║███████╗    
+╚══════╝ ╚═════╝  ╚═════╝╚═╝╚═╝  ╚═╝╚══════╝    
+                                                
+""")
+  
+print(green+"================================≠=================")
+print(yellow+"[~]Contacts in facebook")
+print(blue+"[~]Contacts in Whatsapp")
+print(cyan+"[~]Exit")
+b=input(yellow+"Your Choice:")
+if '1' in b:
+  os.system('clear')
+  time.sleep(1)
+  print(cyan+"""
+               __               _           
+   _________ _/ /_  ____ ______(_)___ ______
+  / ___/ __ `/ __ \/ __ `/ ___/ / __ `/ ___/
+ (__  ) /_/ / / / / /_/ / /  / / /_/ / /    
+/____/\__,_/_/ /_/\__,_/_/  /_/\__,_/_/     
+                                            
+""")
+  
+  
+                                                                   
+  os.system("xdg-open https://www.facebook.com/sahariar.hosen.73")
+
+
+if '2' in b:
+  
+  
+  
+  os.system('clear')
+  time.sleep(1)
+  
+  print(green+"""
+ __    __ _           _                         
+/ / /\ \ \ |__   __ _| |_ ___  __ _ _ __  _ __  
+\ \/  \/ / '_ \ / _` | __/ __|/ _` | '_ \| '_ \ 
+ \  /\  /| | | | (_| | |_\__ \ (_| | |_) | |_) |
+  \/  \/ |_| |_|\__,_|\__|___/\__,_| .__/| .__/ 
+                                   |_|   |_|    
+""") 
+ 
+  time.sleep(1)
+  print("==========================https://whatsapp.com/dl/==================")
+  time.sleep(2)
+  
+  os.system("xdg-open https://whatsapp.com/dl/")
+
+if '3' in b:
+  sys.exit()
+if '4' in a:
+  print(green+"wait few sec....")
+  time.sleep(2)
+  os.system("xdg-open https://www.facebook.com/profile.php?id=100072697534381")
+  
+  
+
+
+  
