8a85f01d77016032d0c033ccf3295af9642f9c3c
sAhA123riAr <95167395+sijad1@users.noreply.github.com>
2022-02-05 16:56:22 +0600

    Add files via upload

---
+++ SHS-44.py
@@ -0,0 +1,182 @@
+import sys
+import time
+
+time.sleep(1)
+
+import os
+end="\033[0m"
+blue="\33[94m"
+red="\033[91m"
+lightblue="\033[94m"
+white="\33[97m"
+yellow="\33[93m"
+green="\033[1;32m"
+cyan="\033[96m"
+end="\033[0m"
+os.system('clear')
+
+print(yellow+""" 
+  _                 _       
+ | |               (_)      
+ | |     ___   __ _ _ _ __  
+ | |    / _ \ / _` | | '_ \ 
+ | |___| (_) | (_| | | | | |
+ |______\___/ \__, |_|_| |_|
+               __/ |        
+              |___/         
+""")
+usern="ls"
+passwd="ls"
+inpuser=str(input(cyan+          "Enter your User[>]"))
+inppass=str(input(cyan+          "Enter your Pass[>]"))
+if usern==inpuser and passwd==inppass:
+  print(green+"[√]Sucsesfull login")
+  time.sleep(1)
+  os.system('clear')
+  pass
+else:
+  sys.exit()
+print(green+"""
+                                                 
+        ,--,                                     
+      ,--.'|            ,--,    ,--,             
+   ,--,  | :          ,--.'|  ,--.'|             
+,---.'|  : '          |  | :  |  | :     ,---.   
+|   | : _' |          :  : '  :  : '    '   ,'\  
+:   : |.'  |   ,---.  |  ' |  |  ' |   /   /   | 
+|   ' '  ; :  /     \  '  | |  '  | |  .   ; ,. : 
+'   |  .'. | /    /  ||  | :  |  | :  '   | |: : 
+|   | :  | '.    ' / |'  : |__'  : |__'   | .; : 
+'   : |  : ;'   ;   /||  | '.'|  | '.'|   :    | 
+|   | '  ,/ '   |  / |;  :    ;  :    ;\   \  /  
+;   : ;--'  |   :    ||  ,   /|  ,   /  `----'   
+|   ,/       \   \  /  ---`-'  ---`-'            
+'---'         `----'                             
+                                                 
+""")
+
+print(green+"===========================================================================")
+print(red+"        [•]Install SHS-44")
+print("                                        ")
+
+print(red+"        [•]See SHS-44 Logo")
+print("                                          ")
+print(red+"        [•]See SHS-44 FB link")
+print("                                          ")
+print(green+"          [×]Exit")
+print("                                  ")
+print(lightblue+"        [•]Go to other tools")
+print("                                  ")
+
+
+
+a=input( yellow+            "Enter your choice:")
+if '1' in a:
+	print(red+"Sorry this pkg not aviable now plz  try another tools [×]''")
+	time.sleep(1)
+	sys.exit()
+
+if '2' in a:
+	os.system('clear')
+	time.sleep(1)
+	print(green+"""
+ .d8888b.  888    888  .d8888b.             d8888      d8888  
+d88P  Y88b 888    888 d88P  Y88b           d8P888     d8P888  
+Y88b.      888    888 Y88b.               d8P 888    d8P 888  
+ "Y888b.   8888888888  "Y888b.           d8P  888   d8P  888  
+    "Y88b. 888    888     "Y88b.        d88   888  d88   888  
+      "888 888    888       "888 888888 8888888888 8888888888 
+Y88b  d88P 888    888 Y88b  d88P              888        888  
+ "Y8888P"  888    888  "Y8888P"               888        888  
+========================================================================================================                                                             
+ """)
+time.sleep(2)
+sys.exit()
+ 
+ 
+
+if '3' in a:
+	
+	os.system('clear')
+	time.sleep(0.5)
+	print(green+""" 
+  ______ ____        _      _       _    
+ |  ____|  _ \      | |    (_)     | |   
+ | |__  | |_) |_____| |     _ _ __ | | __
+ |  __| |  _ <______| |    | | '_ \| |/ /
+ | |    | |_) |     | |____| | | | |   < 
+ |_|    |____/      |______|_|_| |_|_|\_\
+                                         
+                                         
+""")
+if '3' in a:
+  
+  
+ os.system("xdg-open https://www.facebook.com/sahariar.hosen.73")
+ time.sleep(4)
+ os.system('clear')
+ print(yellow+"""
+  ________                __       
+ /_  __/ /_  ____ _____  / /_______
+  / / / __ \/ __ `/ __ \/ //_/ ___/
+ / / / / / / /_/ / / / / ,< (__  ) 
+/_/ /_/ /_/\__,_/_/ /_/_/|_/____/  
+                                   """)
+ 
+ 
+	
+
+if '4' in a:
+	print(red+"Thank you bro ")
+	time.sleep(1)
+	os.system('clear')
+	
+if '5' in a:
+  print(yellow+"wait a few sec....")
+  os.system('clear')
+  time.sleep(2)
+  
+  print(green+"""
+███████╗ █████╗ ██╗  ██╗ █████╗ ██████╗ ██╗ █████╗ ██████╗ 
+██╔════╝██╔══██╗██║  ██║██╔══██╗██╔══██╗██║██╔══██╗██╔══██╗
+███████╗███████║███████║███████║██████╔╝██║███████║██████╔╝
+╚════██║██╔══██║██╔══██║██╔══██║██╔══██╗██║██╔══██║██╔══██╗
+███████║██║  ██║██║  ██║██║  ██║██║  ██║██║██║  ██║██║  ██║
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+                                                           
+""")
+print(cyan+"""
+██╗  ██╗ ██████╗ ███████╗███████╗███╗   ██╗
+██║  ██║██╔═══██╗██╔════╝██╔════╝████╗  ██║
+███████║██║   ██║███████╗█████╗  ██╔██╗ ██║
+██╔══██║██║   ██║╚════██║██╔══╝  ██║╚██╗██║
+██║  ██║╚██████╔╝███████║███████╗██║ ╚████║
+╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═══╝
+                                           
+""")
+                                                                             
+                                                                                  
+print(yellow+"==============================================================")
+
+print(blue+            "[1]phising websit")
+print(blue+            "[2]contact whatsapp")
+print(blue+            "[3]exit")
+c=input(green+            "Enter a choice (1 or 2 or 3) :")
+if '1' in c:
+  time.sleep(1)
+  os.system('clear')
+  os.system("xdg-open https://sniperdz.com/sign-in.php")
+time.sleep(0.5)
+if '2' in c:
+  os.system("xdg-open https://whatsapp.com/dl/")
+
+else:
+  print(red+"[×] Wrong number enterd")
+  time.sleep(2)
+  
+  
+
+
+
+	
+
